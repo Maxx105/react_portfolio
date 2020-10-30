4f7f99a949376cb7b6bc1c046e2837d2e06f4a37
Maxx Sanner <maxxsanner105@gmail.com>
2020-10-29 21:03:11 -0700

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
@@ -14,10 +15,13 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="*" component={NoMatch} />
+          </Switch>
         </Wrapper>
         <Footer />
       </div>
